f8988ac21afd44d0d7b8504d452953ac90d21948
Denis Souza <denis.souza@ulteriorti.com.br>
2022-07-26 20:11:09 -0300

    Dev

--- src/br/com/ulteriorti/desafio/dominio/Dev.java
+++ src/br/com/ulteriorti/desafio/dominio/Dev.java
@@ -1,6 +1,7 @@
 package br.com.ulteriorti.desafio.dominio;
 
 import java.util.LinkedHashSet;
+import java.util.Optional;
 import java.util.Set;
 
 public class Dev {
@@ -9,13 +10,21 @@ public class Dev {
     private Set<Conteudo> conteudosConcluidos = new LinkedHashSet<>();
 
     public void inscreverBootcamp(Bootcam bootcamp){
+        this.conteudosInscritos.addAll(bootcamp.getConteudos());
+        bootcamp.getDevsInscritos().add(this);
 
     }
     public void progredir(){
-
+        Optional<Conteudo> conteudo = this.conteudosInscritos.stream().findFirst();
+        if(conteudo.isPresent()){
+            this.conteudosConcluidos.add(conteudo.get());
+            this.conteudosInscritos.remove(conteudo.get());
+        }else{
+            System.err.println("Você não está matriculado em nenhum conteúdo!");
+        }
     }
-    public void calcularTotalXp(){
-
+    public double calcularTotalXp(){
+        return this.conteudosConcluidos.stream().mapToDouble(Conteudo::calcularXp).sum();
     }
 
     public String getNome() {
